9036ccb265fa7dc41f38f28d8f95efc0ac9df53d
Henry Janke <henry.j@aluno.ifsc.edu.br>
2023-10-24 11:26:11 -0300

    Arquivo bd.txt

--- pinkman-kart/src/main/java/controle/Conexao.java
+++ pinkman-kart/src/main/java/controle/Conexao.java
@@ -1,5 +1,8 @@
 package controle;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -7,9 +10,10 @@ import java.sql.SQLException;
 public class Conexao {
 	private static Connection conexao;
 	private static Conexao instancia;
-	private static final String DATABASE = "diagrama_karts";
-	private static final String USER = "root";
-	private static final String PSW = "aluno";
+
+	private static String DATABASE = null;
+	private static String USER = null;
+	private static String PSW = null;
 
 	public Conexao() {
 	}
@@ -17,17 +21,37 @@ public class Conexao {
 	public static Conexao getInstancia() {
 		if (instancia == null) {
 			instancia = new Conexao();
+			lerArquivoBD();
 		}
 		return instancia;
 	}
 
 	public Connection conectar() {
 		try {
-			conexao = DriverManager.getConnection("jdbc:mysql://localhost/"+ DATABASE + "?serverTimezone=UTC", USER, PSW);
+			conexao = DriverManager.getConnection("jdbc:mysql://localhost/" + DATABASE + "?serverTimezone=UTC", USER,
+					PSW);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return conexao;
+	}
+
+	public static void lerArquivoBD() {
+		// Lê as informações de login e senha do arquivo de texto
+
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader("credentials.txt"));
+			if (reader != null) {
+				DATABASE = reader.readLine();
+				USER = reader.readLine();
+				PSW = reader.readLine();
+			}
+			reader.close();
+
+		} catch (IOException e) {
+			System.err.println("Erro ao ler o arquivo: " + e.getMessage());
+			return;
+		}
 	}
 
 	public boolean fecharConexao() {
