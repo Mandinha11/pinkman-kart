bd3a15c72f34b7feda7998a480bf9c845c7ff09f
lucas <lucas.al2003@aluno.ifsc.edu.br>
2023-10-24 12:03:44 -0300

    Test Unitario Kart

--- pinkman-kart/src/main/java/src/test/java/KartsDAOtest.java
+++ pinkman-kart/src/main/java/src/test/java/KartsDAOtest.java
@@ -5,32 +5,46 @@ import static org.junit.Assert.assertEquals;
 import java.time.LocalDate;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
 
 import controle.KartsDAO;
 import modelo.Karts;
-
+@TestMethodOrder(OrderAnnotation.class)
 public class KartsDAOtest {
 	
 	// annotation
 	@Test
+	@Order(1)
 	
 	public void testMetodoInserirKarts() {
 		Karts k = new Karts();
-		//k.setano(2000l);
-		//k.setCNPJ(12345678901234l);
-		//k.setcor("preto");
-		//k.setdataEntrada(LocalDate.of(2000,12,22));
-		//k.setmarca("Ferrari");
-		//k.setmodelo("modeloA");
-		//k.setpreco(500000l);
-		//k.setquantidade(2l);
-		k.setId(7l);
+		k.setano(2000l);
+		k.setCNPJ(89012340000567l);
+		k.setCor("preto");
+		k.setDataEntrada(LocalDate.of(2000,12,22));
+		k.setMarca("Marca H");
+		k.setModelo("Modelo Y");
+		k.setpreco(500000l);
+		k.setquantidade(2l);
+		k.setId(8l);
 		
+		
+		KartsDAO dao = new KartsDAO();
+		boolean InserirOk = dao.Inserir(k);
+		assertEquals(true, InserirOk);
+	}
+	
+	@Test
+	@Order(2)
+	public void testMetodoDeletarKarts() {
+		Karts k = new Karts();
+		k.setId(8l);
 		
 		KartsDAO dao = new KartsDAO();
 		boolean DeletarOk = dao.Deletar(k);
 		assertEquals(true, DeletarOk);
-	}
-	
+}
 	
 }
