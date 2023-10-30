e6a93be3aaefd56b1679ba7d62d2778ada32dd2b
Henry Janke <henry.j@aluno.ifsc.edu.br>
2023-10-30 17:13:03 -0300

    testMetodoAutenticarLogin() Junity

--- pinkman-kart/src/main/java/src/test/java/LoginDaoTest.java
+++ pinkman-kart/src/main/java/src/test/java/LoginDaoTest.java
@@ -2,13 +2,27 @@ package src.test.java;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.sql.SQLException;
+
 import org.junit.jupiter.api.Test;
+
+import controle.Conexao;
+import controle.LoginDao;
 
 class LoginDaoTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void testMetodoAutenticarLogin() throws SQLException {
+        
+        LoginDao dao = new LoginDao();
 
+      
+        String login = "Funcionario 1"; 
+        Long senha = 12345678900L; 
+       
+        boolean autenticado = dao.autenticarLogin(login, senha);
+
+      
+        assertTrue(autenticado);
+    }
 }
